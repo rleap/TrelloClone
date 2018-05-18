8a47f166a810be853f8eeeafcd9ab13a8b57ca0b
Robert Leap <robertleap@gmail.com>
2018-05-18 00:25:55 -0700

    Added Signup and Login functionality

---
+++ client/src/router.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import SignUp from './views/SignUp.vue';
+import Login from './views/Login.vue';
+import Boards from './views/Boards.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/boards',
+      name: 'boards',
+      component: Boards,
+    },
+  ],
+});
---
+++ client/src/views/Boards.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+          <v-progress-circular
+            :size="70"
+            :width="7"
+            indeterminate
+            color="primary">
+          </v-progress-circular>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
